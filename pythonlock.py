3a52bfefdb5e8cd9997ee513052397f531691157
Sean Reifschneider <sean@realgo.com>
2014-10-07 14:17:42 -0600

    Changing names to be more modern Python.

--- pythonlock.py
+++ pythonlock.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#  Generic locking routines
+#  Generic locking routines for Python
 
 '''
 Just select a lock-file name, and create a jlock instance using that
@@ -8,10 +8,10 @@ name any any parameters you want to override.  Then either let the
 object fall out of scope or (preferred) call the "release()" method:
 
     import jlock
-    lock = jlock.lock('/dev/shm/mylockfile', retries=100, doRaise=True)
-    if not lock.haveLock:
+    lock = jlock.lock('/dev/shm/mylockfile', retries=100, do_raise=True)
+    if not lock.have_lock:
         print "Didn't get lock"
-        print "This shouldn't happen because doRaise=1 is set"
+        print "This shouldn't happen because do_raise=1 is set"
         sys.exit(0)
     [do stuff]
     lock.release()
@@ -22,15 +22,18 @@ The lock sleeps for one second, so the above will wait for the lock for
 "retries" is the number of attempts that will be made before the locking
 code gives up trying to get the lock.
 
-"doRaise" if raise an exception if the lock cannot be aquired.
+"do_raise" if raise an exception if the lock cannot be aquired.
 
-"tryBreak" if set will cause the code to read the lock-file and if the
+"try_break" if set will cause the code to read the lock-file and if the
 process ID listed in it is no longer running, the lock-file will be
 deleted.
 
-"sleepTime" is the number of seconds between retries.  This can be
+"sleep_time" is the number of seconds between retries.  This can be
 floating-point, as provided to "time.sleep()".
 '''
+
+import os
+import time
 
 
 class LockError(Exception):
@@ -40,77 +43,70 @@ class LockError(Exception):
 
 class lock:
     def __init__(
-            self, name, retries=0, tryBreak=True, doRaise=False,
-            sleepTime=1):
-        import os
-        import time
-
-        self.lockedByPid = None
+            self, name, retries=0, try_break=True, do_raise=False,
+            sleep_time=1):
+        #  imports
+        self.locked_by_pid = None
         self.pid = str(os.getpid())
-        self.lockFile = name
-        self.lockFile2 = self.lockFile + '.' + self.pid
-        self.haveLock = False
-        self.sleepTime = sleepTime
+        self.lock_file = name
+        self.lock_file2 = self.lock_file + '.' + self.pid
+        self.have_lock = False
+        self.sleep_time = sleep_time
 
-        while not self.haveLock:
-            if tryBreak and os.path.exists(self.lockFile):
+        while not self.have_lock:
+            if try_break and os.path.exists(self.lock_file):
                 self.break_existing_lock()
 
             #  try to obtain the lock
             try:
                 #  write our PID to our lock-file
-                fp = open(self.lockFile2, 'w')
+                fp = open(self.lock_file2, 'w')
                 fp.write('%s\n' % self.pid)
                 fp.close()
 
-                os.link(self.lockFile2, self.lockFile)
-                self.haveLock = True
+                os.link(self.lock_file2, self.lock_file)
+                self.have_lock = True
             except OSError:
                 if retries > 0:
-                    time.sleep(self.sleepTime)
+                    time.sleep(self.sleep_time)
 
             #  exit if we aren't supposed to wait
             if retries < 1:
                 break
             retries = retries - 1
 
-        if not self.haveLock:
-            os.unlink(self.lockFile2)
-            if doRaise:
+        if not self.have_lock:
+            os.unlink(self.lock_file2)
+            if do_raise:
                 raise(LockError, "Unable to obtain lock")
 
     def break_existing_lock(self):
-        import os
-        import time
-
-        self.lockedByPid = open(self.lockFile, 'r').readline().strip()
+        self.locked_by_pid = open(self.lock_file, 'r').readline().strip()
         try:
-            self.lockedByPid = int(self.lockedByPid)
+            self.locked_by_pid = int(self.locked_by_pid)
         except ValueError:
-            self.lockedByPid = None
+            self.locked_by_pid = None
         try:
-            if self.lockedByPid is not None:
-                os.kill(int(self.lockedByPid), 0)
+            if self.locked_by_pid is not None:
+                os.kill(int(self.locked_by_pid), 0)
         except OSError:
             try:
-                os.unlink(self.lockFile + '.' + self.lockedByPid)
+                os.unlink(self.lock_file + '.' + self.locked_by_pid)
             except OSError:
                 pass
             try:
-                os.unlink(self.lockFile)
+                os.unlink(self.lock_file)
             except OSError:
                 pass
-            time.sleep(self.sleepTime)
+            time.sleep(self.sleep_time)
 
     def release(self):
-        import os
-
         try:
-            if os.path.exists(self.lockFile2):
-                os.unlink(self.lockFile2)
-            if self.haveLock and os.path.exists(self.lockFile):
-                os.unlink(self.lockFile)
-            self.haveLock = False
+            if os.path.exists(self.lock_file2):
+                os.unlink(self.lock_file2)
+            if self.have_lock and os.path.exists(self.lock_file):
+                os.unlink(self.lock_file)
+            self.have_lock = False
         except OSError:
             pass
 
